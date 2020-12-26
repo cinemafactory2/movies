1524ef446b4ff2084f8f60ff2fcfbf9acb00ff46
cinemafactory2 <73420844+cinemafactory2@users.noreply.github.com>
2020-12-26 09:43:37 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Docusaurus: ['christmas wishes', 'doc2', 'doc3'],
     Features: ['edapazhassi media'],
   },
 };
